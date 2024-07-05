8802bc021607fb4950c2de93a0420c75af86d05b
vedantjain8 <vedantjain88666@gmail.com>
2024-07-05 21:49:47 +0530

    changed from g4f to manual task

--- getQuoteListFromAI.py
+++ getQuoteListFromAI.py
@@ -1,46 +1,46 @@
 from random import choice
-import g4f
-import g4f.client
+# import g4f
+# import g4f.client
 
 
-contentList = [
-    "give me a 30-60 seconds amazing facts on a random topic, make sure to make it interesting",
-    "give me a 40-60 seconds script on a single real mystifying marvels of enginerring , make sure to make it interesting",
-]
+# contentList = [
+#     "give me a 30-60 seconds amazing facts on a random topic, make sure to make it interesting",
+#     "give me a 40-60 seconds script on a single real mystifying marvels of enginerring , make sure to make it interesting",
+# ]
 
-positiveParameters = "give a list of 4 keywords to search for bit sized video clips from pexels. Return plain text output in the given format: output: output than keywords: csv of keywords. make keyword comma seperated"
+# positiveParameters = "give a list of 4 keywords to search for bit sized video clips from pexels. Return plain text output in the given format: output: output than keywords: csv of keywords. make keyword comma seperated"
 
-def getQuoteListFromAI(topic=None):
-    '''
-    This function generates a list of quotes or facts from the AI.
-    
-    Args:
-        topic (str): The topic to generate the quotes on. Default is None.
-    
-    Returns:
-        list: The list of quotes or facts.
-    '''
+# def getQuoteListFromAI(topic=None):
+#     '''
+#     This function generates a list of quotes or facts from the AI.
 
-    if (topic == None):
-        topic = choice(contentList)
-    else:
-        topic = topic
-    # outputs string of text
-    engine = g4f.client.Client()
+#     Args:
+#         topic (str): The topic to generate the quotes on. Default is None.
 
-    completion = engine.chat.completions.create(
-        model="gpt-4",
-        # model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": topic + "." + positiveParameters}])
+#     Returns:
+#         list: The list of quotes or facts.
+#     '''
 
-    output = completion.choices[0].message.content
-    return output
+#     if (topic == None):
+#         topic = choice(contentList)
+#     else:
+#         topic = topic
+#     # outputs string of text
+#     engine = g4f.client.Client()
+
+#     completion = engine.chat.completions.create(
+#         model="gpt-4",
+#         # model="gpt-3.5-turbo",
+#         messages=[{"role": "user", "content": topic + "." + positiveParameters}])
+
+#     output = completion.choices[0].message.content
+#     return output
 
 
 def cleanTextOut(inputTextList):
     '''
     This function formates the text output from the AI.
-    
+
     Args:
         inputTextList (list): The list of text output from the AI.
 
